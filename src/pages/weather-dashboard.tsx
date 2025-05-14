98b7697201035732d4ce772290c7e716cd1bf97c
Vic428-human <z0983195379a@gmail.com>
2025-05-14 08:03:26 +0800

    4.對我的位置跟刷新功能切版 5.並調用位置確認hook正常

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { useGeoLocation } from "@/hooks/use-geolocation";
+import { RefreshCw } from "lucide-react";
 import React from "react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard!</div>;
+  // create custom hook for fetching my current location
+  const {
+    getLocation,
+    coordinates,
+    error,
+    isLoading: locationLoading,
+  } = useGeoLocation();
+  // console.log("coordinates===>", { coordinates });
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      console.log("coordinates===>", { coordinates });
+    }
+  };
+
+  if (locationLoading) {
+  }
+  return (
+    <div className="space-y-4">
+      {/* favorite city  */}
+      <div className="flex items-center justify-between ">
+        {/* tracking-tight
+      letter-spacing: var(--tracking-tight); /* -0.025em */}
+        <h1 className="text-xl font-medium tracking-tight">My Location</h1>
+        <Button
+          variant="outline"
+          size="icon"
+          // onClick={handleRefresh}
+          // disabled={false}
+        >
+          <RefreshCw className="h-5 w-5" />
+        </Button>
+      </div>
+      {/* get current and hourly weather */}
+    </div>
+  );
 };
 
 export default WeatherDashboard;
