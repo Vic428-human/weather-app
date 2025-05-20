86e1639e5def0e9d0d604da2b9dd4cfb80151c66
Vic428-human <z0983195379a@gmail.com>
2025-05-20 20:11:35 +0800

    1.RWD:調整搜尋按鈕在不同模式下的寬度變化 2.切版：區分三種heading，例如suggestion跟 Favorites 3. 判斷地區的顯示邏輯，載入時顯示Loader 4. 搜尋到資料時顯示 CommandItem 的列表 5.點選某個特定CommandItem 時把資料導入給新的Router 6.傳送的參數值，透過location裡的座標位置跟名稱和國家 7.顯示location的清單，區分名稱/州/國家三種

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -1,5 +1,108 @@
+import { useState } from "react";
+import { Search, Loader2 } from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { Button } from "./ui/button";
+import { useLocationSearch } from "@/hooks/use-weather";
+import { useNavigate } from "react-router-dom";
+
 const CitySeatch = () => {
-  return <div>CitySeatch</div>;
+  const [open, setOpen] = useState(false);
+  const [query, setQuery] = useState("");
+  const { data: locations, isLoading } = useLocationSearch(query);
+  // <Route element={<CityPage />} path="/city/:cityName"></Route>
+  // 之前有定義過另一個router頁面，:cityName 這就是接下來要取得的 params
+
+  const navigate = useNavigate();
+
+  const handleSelectChange = (cityData: string) => {
+    // cityData===> 22.2793278|114.1628131|Hong Kong Island|CN
+    // 之前用 | 分隔，現在透過 | 來split 出數據
+    const [lat, long, name, country] = cityData.split("|");
+    console.log(lat, long, name, country);
+    // 做某些事情的時候切換到Count頁面
+    navigate(`/city/${name}??lat=${lat}&lon=${long}`); // 例如： 搜尋 new taipei => New%20Taipei??lat=25.012&lon=121.45 => %20 === 空格
+  };
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(true)}
+        variant="outline"
+        // 在不同的RWD模式，調整按鈕組件不同 width
+        className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-60"
+      >
+        <Search className="mr-2 h-4 w-4" />
+        Search Cities
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput
+          placeholder="Search Cities"
+          value={query}
+          onValueChange={setQuery}
+        />
+        <CommandList>
+          {/* 查詢的關鍵字不存在時 */}
+          {query.length > 2 && !isLoading && (
+            <CommandEmpty>No Cities found.</CommandEmpty>
+          )}
+          <CommandGroup heading="Favorites">
+            <CommandItem>Calendar</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Recent Seatches">
+            <CommandItem>Calendar</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          {locations && locations.length > 0 && (
+            <CommandGroup heading="Suggestions">
+              {isLoading && (
+                <div className="flex items-center justify-center p-4">
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                </div>
+              )}
+              {locations.map((location) => {
+                return (
+                  <CommandItem
+                    // https://github.com/shadcn-ui/ui/issues/889
+                    key={`${location.lat}-${location.lon}`}
+                    value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                    // Instead of depending on the onSelect val, just use the value from .map
+                    // handleSelectChange 傳遞的參數會是先前 CommandItem 的 value
+                    onSelect={handleSelectChange}
+                  >
+                    <Search className="mr-2 h-4 w-4" />
+                    <span className="text-sm text-muted-foreground">
+                      地區：{location.name}
+                    </span>
+
+                    {/* 哪一州 */}
+                    {location.state && (
+                      <span className="text-sm text-muted-foreground">
+                        , 州： {location.state}
+                      </span>
+                    )}
+                    {/* 哪一國 */}
+                    <span className="text-sm text-muted-foreground">
+                      , 國：{location.country}
+                    </span>
+                  </CommandItem>
+                );
+              })}
+            </CommandGroup>
+          )}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
 };
 
 export default CitySeatch;
