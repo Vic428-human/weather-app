2b453281e54f82e01b741df7352f6691aa8d6c87
Vic428-human <z0983195379a@gmail.com>
2025-05-23 03:42:57 +0800

    2.獨立出 FavoriteButton 組件，我的最愛組件可以toggle 添加喜歡跟取消喜歡的業務邏輯 3.對我的最愛增加styling

---
+++ src/components/favorite-button.tsx
@@ -0,0 +1,43 @@
+import type { WeatherResponse } from "@/api/type";
+import { useFavorite } from "@/hooks/use-favoriate";
+import { Star } from "lucide-react";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+
+interface FavoriteWeatherProps {
+  data: WeatherResponse;
+}
+
+const FavoriteButton = ({ data }: FavoriteWeatherProps) => {
+  const { ifFavorite, addFavorite, removeFavorite } = useFavorite();
+  const isCurrentFavorite = ifFavorite(data.coord.lat, data.coord.lon);
+
+  const handleToggleFavorite = () => {
+    if (isCurrentFavorite) {
+      removeFavorite.mutate(`${data.coord.lat}-${data.coord.lon}`);
+      toast.warning(`Remove ${data.name} from favorites`);
+    } else {
+      addFavorite.mutate({
+        name: data.name,
+        lat: data.coord.lat,
+        lon: data.coord.lon,
+        country: data.sys.country,
+      });
+      // https://github.com/shadcn-ui/ui/issues/2234
+      toast.success(`Added ${data.name} to favorites`);
+    }
+  };
+  return (
+    <Button
+      variant={isCurrentFavorite ? "default" : "outline"}
+      size="icon"
+      className={isCurrentFavorite ? "bg-yellow-500 hover:bg-yellow-600" : ""}
+      onClick={handleToggleFavorite}
+    >
+      {/* Use the fill-current utility to set the fill color to the current text color: */}
+      <Star className={`h-4 w-4 ${isCurrentFavorite ? " fill-current" : ""}`} />
+    </Button>
+  );
+};
+
+export default FavoriteButton;
